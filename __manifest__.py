ad3a264051da17d2884d2729812cdcfda0ac4666
DelphineLemire <delphine.lemire@gmail.com>
2023-09-17 16:46:29 +0200

    add theme models

--- __manifest__.py
+++ __manifest__.py
@@ -12,6 +12,7 @@
 
     'author': "Delphine Lemire",
     'website': "https://www.delphinelemire.fr",
+    'license': "AGPL-3", 
     'installable': True,
     'application': True,
     
@@ -27,8 +28,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'security/ir.model.access.csv',
+        'views/themes.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
